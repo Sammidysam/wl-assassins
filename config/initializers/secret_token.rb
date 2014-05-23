36d774ab8cf49b7a8b4a0f9e49d0cd95aab8efd3
Sammidysam <sammidysam@gmail.com>
2014-05-23 11:07:25 -0400

    Create Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WlAssassins::Application.config.secret_key_base = '0aa3ccf02ec2954a778830bcc285f02793b20acff53f5ea3545884677c04ca245a3135f5baaab4c9625b87a54d46ae4787475da811d07b1c668ddafeac80ac2a'
