470a9889d99b6108dd1af4e37c22d7318b306005
Sammidysam <sammidysam@gmail.com>
2014-05-31 20:16:33 -0400

    Fix up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 WlAssassins::Application.routes.draw do
-  get "dashboard/index"
+  get "dashboard" => "dashboard/index"
+	
   resources :teams
-
   resources :users
-
   resources :pages
-
   resources :games
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root "welcome#index"
+  root "dashboard"
 
   # Example of regular route:
   #   get "products/:id" => "catalog#view"
