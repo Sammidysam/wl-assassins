3de531edb9007fc89018ef2da256df4413a18c96
Sammidysam <sammidysam@gmail.com>
2014-06-02 12:48:26 -0400

    Allow controller to accept new user fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def user_params
-		params.require(:user).permit(:name, :email, :password_digest, :phone_number, :phone_number_public, :address, :address_public, :graduation_year, :description, :profile_picture_url, :admin, :out_of_town, :willing_to_pay_amount, :paid_amount)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone_number, :phone_number_public, :address, :address_public, :graduation_year, :description, :profile_picture_url, :admin, :out_of_town, :willing_to_pay_amount, :paid_amount)
 	end
 end
