8c8607dc060b28b759526d318b115c2dad0869ce
Sammidysam <sammidysam@gmail.com>
2014-06-02 12:50:29 -0400

    Indent Session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,15 +1,15 @@
 class SessionController < ApplicationController
-  def new
-  end
+	def new		
+	end
 
-  def create
-  end
+	def create
+	end
 
-  def destroy
-  end
-  def create
-  end
+	def destroy
+	end
+	def create
+	end
 
-  def destroy
-  end
+	def destroy
+	end
 end
