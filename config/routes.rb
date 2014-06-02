3bba4f6bb9102b1cccd7a8c222db80c5f9a24d86
Sammidysam <sammidysam@gmail.com>
2014-06-02 12:52:48 -0400

    Re-create Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 WlAssassins::Application.routes.draw do
+  get "session/new"
+  get "session/create"
+  get "session/destroy"
 	get "session/new"
 	get "session/destroy"
 	post "session/create" => "session#create"
