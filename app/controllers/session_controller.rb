f611d5230177fa709ffdbed1536ee39dab716998
Sammidysam <sammidysam@gmail.com>
2014-06-02 12:56:36 -0400

    Add Session controller logic

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,31 @@
 class SessionController < ApplicationController
-  def new
-  end
+	def new
+		redirect_to root_path if current_user
+	end
 
-  def create
-  end
+	def create
+		fail_message = "Failed to log in!"
 
-  def destroy
-  end
+		users = User.where :email => params["email"]
+
+		if users.count > 0
+			@user = users.first
+
+			if @user.authenticate params["password"]
+				session["current_user_id"] = @user.id
+
+				redirect_to root_path
+			else
+				redirect_to session_new_path, :alert => fail_message
+			end
+		else
+			redirect_to session_new_path, :alert => fail_message
+		end
+	end
+
+	def destroy
+		session["current_user_id"] = nil
+
+		redirect_to root_path
+	end
 end
