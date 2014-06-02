3b658f46f1acfed6f60ea0cbcc232512f1f1ccb6
Sammidysam <sammidysam@gmail.com>
2014-06-02 13:54:22 -0400

    Clean up routes, change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 WlAssassins::Application.routes.draw do
-	get "welcome/index"
-	get "session/new"
+	get "dashboard" => "dashboard#index"
 	get "session/destroy"
+	get "session/new"
+	get "welcome/index"
+	
 	post "session/create" => "session#create"
 	post "session/new"
 	
-	get "dashboard" => "dashboard#index"
-	
+	resources :games
+	resources :pages
 	resources :teams
 	resources :users
-	resources :pages
-	resources :games
 
 	# The priority is based upon order of creation: first created -> highest priority.
 	# See how all your routes lay out with "rake routes".
 
 	# You can have the root of your site routed with "root"
-	root "dashboard#index"
+	root "welcome#index"
 
 	# Example of regular route:
 	#   get "products/:id" => "catalog#view"
