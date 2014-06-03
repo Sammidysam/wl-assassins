96fc0fe17361e3892ac671749ec1bb83eb9163ab
Sammidysam <sammidysam@gmail.com>
2014-06-03 10:36:21 -0400

    Add rendering the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		redirect_to(dashboard_path) if view_context.current_user
+
+		@page = Page.where("name LIKE ?", "welcome").first
 	end
 end
