7d41c5acfe47ab007282aa1c709a81e85ebd303a
Sammidysam <sammidysam@gmail.com>
2014-06-03 10:47:04 -0400

    Better way of grabbing welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
 	def index
 		redirect_to(dashboard_path) if view_context.current_user
 
-		@page = Page.where("name LIKE ?", "welcome").first
+		@page = Page.find_by(welcome: true)
 	end
 end
