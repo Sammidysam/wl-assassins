528de0a043b0f5195e54eb1f64027b75bffaff9e
Sammidysam <sammidysam@gmail.com>
2014-06-03 10:58:45 -0400

    Remove parentheses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		if view_context.current_user
-			redirect_to(dashboard_path)
+			redirect_to dashboard_path
 		else
 			@page = Page.find_by welcome: true
 		end
