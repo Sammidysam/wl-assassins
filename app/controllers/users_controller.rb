e00bb555bef46fba018fc25ad9905febb5e8d5c2
Sammidysam <sammidysam@gmail.com>
2014-06-03 11:39:23 -0400

    Log out before deleting the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,11 @@ class UsersController < ApplicationController
 	# DELETE /users/1
 	# DELETE /users/1.json
 	def destroy
+		# Log out before destroying the current user.
+		session["current_user_id"] = nil
+		
 		@user.destroy
+		
 		respond_to do |format|
 			format.html { redirect_to users_url }
 			format.json { head :no_content }
