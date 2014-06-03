2aadcf2bc498e74050717001e948dd5ee727fa90
Sammidysam <sammidysam@gmail.com>
2014-06-03 11:30:01 -0400

    Better validation error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
 	validates :password, confirmation: true, presence: true, on: :create
 	validates :email, :name, :phone_number, :address, :graduation_year, :profile_picture_url, :willing_to_pay_amount, presence: true
-	validates :email, uniqueness: true, email_format: { message: "incorrect format" }
+	validates :email, uniqueness: true, email_format: { message: "is not valid" }
 	validates :graduation_year, numericality: { greater_than_or_equal_to: Date.today.year, less_than_or_equal_to: Date.today.year + 4 }
-	validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}|\d{3}-\d{4}/, message: "incorrect format" }
+	validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}|\d{3}-\d{4}/, message: "has an incorrect format" }
 end
