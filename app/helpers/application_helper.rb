4492130dbe9e47126933d60f90e93c6e8102d99c
Sammidysam <sammidysam@gmail.com>
2014-06-03 10:24:51 -0400

    Add ability to specify title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def current_user
 		User.find(session["current_user_id"]) if session["current_user_id"]
 	end
+
+	def title(title_string)
+		content_for :title, title_string.to_s
+	end
 end
