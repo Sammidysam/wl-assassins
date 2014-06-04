a9efd622e5592d29f094706029823250cab50c0a
Sammidysam <sammidysam@gmail.com>
2014-06-04 12:18:15 -0400

    Remove now-useless comment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
 		if view_context.current_user
 			redirect_to dashboard_path
 		else
-			# The welcome page has a special sort index of 0.
 			@page = Page.find_by sort_index: SortIndex::WELCOME
 		end
 	end
