0f4a0d04a5cbd53c0af13b896a2241f69a1e1a1e
Sammidysam <sammidysam@gmail.com>
2014-06-04 11:49:33 -0400

    Fix welcome controller logic
    Now it will account for the new way of getting the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,8 @@ class WelcomeController < ApplicationController
 		if view_context.current_user
 			redirect_to dashboard_path
 		else
-			@page = Page.find_by welcome: true
+			# The welcome page has a special sort index of 0.
+			@page = Page.find_by sort_index: 0
 		end
 	end
 end
