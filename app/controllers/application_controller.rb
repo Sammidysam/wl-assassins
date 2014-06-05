036e6774912ca1a8c9ef13707ac44dad9045cafb
Sammidysam <sammidysam@gmail.com>
2014-06-05 10:30:25 -0400

    Alias current_user to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
+
+	# Call the helper current_user method.
+	def current_user
+		view_context.current_user
+	end
 end
