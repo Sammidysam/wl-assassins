b0c8b292a5c16febfce4c20879313ee28a215162
Sammidysam <sammidysam@gmail.com>
2014-06-05 12:24:59 -0400

    Add render string again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605160047) do
+ActiveRecord::Schema.define(version: 20140605162447) do
 
   create_table "contracts", force: true do |t|
     t.boolean  "completed"
