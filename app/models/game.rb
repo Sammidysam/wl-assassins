2b953884fdf55b66a1a740d939aea2d862f1b7ff
Sammidysam <sammidysam@gmail.com>
2014-06-08 13:38:08 -0400

    Add completed? method to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,8 @@ class Game < ActiveRecord::Base
 	has_many :contracts, through: :participations
 	has_many :kills, through: :participations
 	has_many :teams, through: :participations
+
+	def completed?
+		!self.ended_at.nil? && !self.in_progress
+	end
 end
