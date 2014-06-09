b317ed3dd67d63bc40b08111ff6a5c2d6c2fee5b
Sammidysam <sammidysam@gmail.com>
2014-06-09 09:57:04 -0400

    Add members method to Team

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,9 @@ class Team < ActiveRecord::Base
 	has_many :users, through: :memberships
 
 	validates :name, presence: true
+
+	# Returns only the user who are current members of team.
+	def members
+		self.memberships.map { |membership| membership.user if membership.active }
+	end
 end
