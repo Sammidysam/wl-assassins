56b7f22714c2396ad4e8f0442857b7f913c5b489
Sammidysam <sammidysam@gmail.com>
2014-06-09 10:21:15 -0400

    Only put users that can be read in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 	# GET /users
 	# GET /users.json
 	def index
-		@users = User.all
+		@users = User.all.select { |user| can? :read, user }
 	end
 
 	# GET /users/1
