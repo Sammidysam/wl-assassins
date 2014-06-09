a7cf2b23496eaf71d5a5dc3c626e878bba265166
Sammidysam <sammidysam@gmail.com>
2014-06-09 09:42:18 -0400

    Remove completed? method in Game
    As it is not currently being used, it is better not to define it.

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,8 +5,4 @@ class Game < ActiveRecord::Base
 	has_many :contracts, through: :participations
 	has_many :kills, through: :participations
 	has_many :teams, through: :participations
-
-	def completed?
-		!self.ended_at.nil? && !self.in_progress
-	end
 end
