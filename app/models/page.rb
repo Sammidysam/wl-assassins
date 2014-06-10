b8da475ce7b458409fc7670f22d3a3ec3549f9d3
Sammidysam <sammidysam@gmail.com>
2014-06-09 22:08:30 -0400

    Fix link not getting set to nil

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ActiveRecord::Base
+	before_validation do
+		self.link = nil if self.link && self.link.empty?
+	end
+	
 	validates :name, presence: true
 	validates :name, :sort_index, uniqueness: true
 end
