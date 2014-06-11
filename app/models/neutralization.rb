355d435dfbc5f496494487af8e20b7d1a60223ee
Sammidysam <sammidysam@gmail.com>
2014-06-11 10:34:21 -0400

    Add method to get end of neutralization

--- app/models/neutralization.rb
+++ app/models/neutralization.rb
@@ -3,4 +3,9 @@ class Neutralization < ActiveRecord::Base
 	
 	belongs_to :killer, class_name: "User"
 	belongs_to :target, class_name: "User"
+
+	# Returns the end of the neutralization.
+	def end
+		start + 1.day
+	end
 end
