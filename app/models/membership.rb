f7cc00d30205607690ddf925a1c0aec7e9dc90c4
Sammidysam <sammidysam@gmail.com>
2014-06-12 12:18:12 -0400

    Admins cannot join teams

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,9 +2,15 @@ class Membership < ActiveRecord::Base
 	belongs_to :team
 	belongs_to :user
 
+	validate :admins_cannot_be_on_team
 	validate :user_cannot_be_on_team_twice
 
 	validates :team_id, :user_id, presence: true
+
+	# Ensures that no admins are on a team.
+	def admins_cannot_be_on_team
+		errors.add :user_id, "cannot be an admin" if User.find(user_id).admin?
+	end
 
 	# Ensure that a user is not actively on the same team twice.
 	# Retrieves all of the memberships matching these conditions, then checks how many besides this membership exist.
