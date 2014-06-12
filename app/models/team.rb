574a77bc9e6d5e065c0611f34b144f2eed2f0aa5
Sammidysam <sammidysam@gmail.com>
2014-06-12 12:34:05 -0400

    Fix max of four users validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,15 +5,9 @@ class Team < ActiveRecord::Base
 	has_many :games, through: :participations
 	has_many :users, through: :memberships
 	
-	validate :member_count_cannot_be_greater_than_four
-	
 	validates :name, presence: true, uniqueness: true
 
 	nilify_blanks
-
-	def member_count_cannot_be_greater_than_four
-		errors.add :users, "cannot have a count greater than four" if members.count > 4
-	end
 
 	# Returns only the user who are current members of team.
 	def members
