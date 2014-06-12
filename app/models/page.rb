80f5c8064d66897c3f6456032f9057fad1560cec
Sammidysam <sammidysam@gmail.com>
2014-06-12 00:00:53 -0400

    Do not nilify content

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-	nilify_blanks
+	nilify_blanks except: :content
 	
 	validates :name, presence: true, uniqueness: true
 	validates :sort_index, uniqueness: true, allow_nil: true
