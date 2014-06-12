8b645e2b4a56708fde3df4286c884ffd423e9ca4
Sammidysam <sammidysam@gmail.com>
2014-06-12 00:01:22 -0400

    This feels more right

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-	nilify_blanks except: :content
+	nilify_blanks
 	
 	validates :name, presence: true, uniqueness: true
 	validates :sort_index, uniqueness: true, allow_nil: true
