b472023c3be0f5c5a50af45d04ea5af3ac905a70
Sammidysam <sammidysam@gmail.com>
2014-06-13 17:09:26 -0400

    Add users and suggest_team_fee methods to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,4 +11,13 @@ class Game < ActiveRecord::Base
 	def completed?
 		self.ended_at && !self.in_progress
 	end
+
+	# All of the users in the game.
+	def users
+		self.teams.map { |team| team.members }.flatten
+	end
+
+	def suggested_team_fee
+		users.empty? ? 0.0 : (users.map { |user| user.willing_to_pay_amount }.sum / users.size.to_f * 4.0)
+	end
 end
