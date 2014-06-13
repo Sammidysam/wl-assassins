2489ca5aec127fbe2568137ef29ad4c333ff1bbf
Sammidysam <sammidysam@gmail.com>
2014-06-13 10:49:21 -0400

    Undo better way of specifying game's terminators
    It just won't work.

--- db/migrate/20140613143449_remove_terminators_from_participation.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTerminatorsFromParticipation < ActiveRecord::Migration
-  def change
-    remove_column :participations, :terminators, :boolean
-  end
-end
--- db/migrate/20140613143513_add_terminators_id_to_game.rb
+++
@@ -1,5 +0,0 @@
-class AddTerminatorsIdToGame < ActiveRecord::Migration
-  def change
-    add_column :games, :terminators_id, :integer
-  end
-end
