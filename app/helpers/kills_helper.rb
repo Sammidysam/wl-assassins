74b6e9d15dbcaca16f0747208247507aba079d3a
Sammidysam <sammidysam@gmail.com>
2014-06-14 11:36:14 -0400

    Add default_participation_id method

--- app/helpers/kills_helper.rb
+++ app/helpers/kills_helper.rb
@@ -1,2 +1,14 @@
 module KillsHelper
+	def default_participation_id(kind, game_id)
+		case kind
+		when "assassination"
+			current_user.team.participation.id
+		when "termination"
+			participation = Participation.find_by(game_id: game_id, terminators: true)
+			
+			(current_user.team && current_user.team.terminators?) ? current_user.team.participation.id : (participation ? participation.id : nil)
+		else
+			nil
+		end
+	end
 end
