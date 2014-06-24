e23e42bab90ca4a789865b68845d121153ea329e
Sammidysam <sammidysam@gmail.com>
2014-06-24 13:19:55 -0400

    Add contract_order_teams method to Games helper
    This helps a lot with displaying all of the contracts at once.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,15 @@
 module GamesHelper
+	# Returns the non-eliminated teams in the game game in order of contracts.
+	def contract_order_teams(game)
+		teams = []
+		starter_team = game.teams.select { |team| !team.eliminated? }.first
+
+		teams << starter_team
+
+		while (next_team = next_team ? next_team.contract.target : starter_team.contract.target).id != starter_team.id
+			teams << next_team
+		end
+
+		teams
+	end
 end
