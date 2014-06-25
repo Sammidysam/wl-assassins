64f78ed94b0425779cbcf44fccc758bca11cd96d
Sammidysam <sammidysam@gmail.com>
2014-06-25 12:24:44 -0400

    Do not add terminators into contract order

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,7 +2,7 @@ module GamesHelper
 	# Returns the non-eliminated teams in the game game in order of contracts.
 	def contract_order_teams(game)
 		teams = []
-		starter_team = game.teams.select { |team| !team.eliminated? }.first
+		starter_team = game.teams.select { |team| !team.terminators? && !team.eliminated? }.first
 
 		if starter_team
 			teams << starter_team
