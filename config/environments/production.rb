a61ea0864954b256928a28d2815cb532f1d59c62
Sammidysam <sammidysam@gmail.com>
2014-06-27 00:50:18 -0400

    Remove workless code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ WlAssassins::Application.configure do
 	# Use default logging formatter so that PID and timestamp are not suppressed.
 	config.log_formatter = ::Logger::Formatter.new
 
-	config.after_initialize do
-		Delayed::Job.scaler = :heroku_cedar
-	end
+	# config.after_initialize do
+	# 	Delayed::Job.scaler = :heroku_cedar
+	# end
 end
