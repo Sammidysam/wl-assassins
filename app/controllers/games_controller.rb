70110ff1ff5eb7a219c36df0d47a0daaffd23be6
Sammidysam <sammidysam@gmail.com>
2014-06-26 22:05:45 -0400

    Temporarily disable autotermination
    I need to test Heroku's ability to handle jobs.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -151,7 +151,7 @@ class GamesController < ApplicationController
 				end
 
 				# Create jobs for team auto-termination.
-				contract_teams.each { |team| team.autoterminate }
+				#contract_teams.each { |team| team.autoterminate }
 				
 				redirect_to @game, alert: (errors ? "Could not set up game!" : nil)
 			else
