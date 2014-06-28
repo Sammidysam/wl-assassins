71454197f885ba1c3378ce6833b5647ab61691c7
Sammidysam <sammidysam@gmail.com>
2014-06-27 21:02:23 -0400

    Add next_non_eliminated_target method to Contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,4 +3,15 @@ class Contract < ActiveRecord::Base
 	belongs_to :target, class_name: "Team"
 
 	validates :participation_id, :target_id, :start, presence: true
+
+	# Returns the next team in the order that is not eliminated.
+	def next_non_eliminated_target
+		contract = self
+
+		while !contract.target.eliminated?
+			contract = contract.target.contract
+		end
+
+		contract
+	end
 end
