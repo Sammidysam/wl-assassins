0522db7489e6d93e6cf9e921d82f6ee177b53043
Sammidysam <sammidysam@gmail.com>
2014-07-09 22:28:47 -0400

    Add methods for precise distance of time in words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,29 @@ module ApplicationHelper
 			teams
 		end
 	end
+
+	def precise_distance_of_time_in_words(from_time, to_time, no_time = false)
+		return "no time" if no_time && from_time < to_time
+		
+		from_time = from_time.to_time if from_time.respond_to?(:to_time)
+		to_time = to_time.to_time if to_time.respond_to?(:to_time)
+		distance_in_seconds = ((to_time - from_time).abs).round
+		components = []
+
+		%w(year month week day hour minute).each do |interval|
+			# For each interval type, if the amount of time remaining is greater than
+			# one unit, calculate how many units fit into the remaining time.
+			if distance_in_seconds >= 1.send(interval)
+				delta = (distance_in_seconds / 1.send(interval)).floor
+				distance_in_seconds -= delta.send(interval)
+				components << pluralize(delta, interval)
+			end
+		end
+
+		components.to_sentence
+	end
+
+	def precise_distance_of_time_in_words_to_now(from_time, no_time = false)
+		precise_distance_of_time_in_words from_time, DateTime.now, no_time
+	end
 end
