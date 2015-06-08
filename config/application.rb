f32aa180bfc37a13d613371c074b13863e4fa8ee
Sammidysam <sammidysam@gmail.com>
2015-06-07 20:40:45 -0400

    Turn off SQL logging for now

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module WlAssassins
 		# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
 		# config.time_zone = 'Central Time (US & Canada)'
 		config.time_zone = "Indiana (East)"
-		
+
 
 		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -24,5 +24,10 @@ module WlAssassins
 
 		# Automatically load lib/ modules, so that I can Include them.
 		config.autoload_paths << Rails.root.join("lib")
+
+		# SQL logging is usually annoying; I know how awfully slow my app is.
+		config.after_initialize do
+            ActiveRecord::Base.logger = nil
+        end
 	end
 end
